fc28d246536d811641b98ccaa7f888fc754603de
hpazk <hpazk.zkh@gmail.com>
2021-01-26 14:09:23 +0700

    Revert "integration"
    This reverts commit 50718bcbfef48d31a209b5757c51e953ae917a3b.

--- vendor/github.com/hpazk/go-echo-rest-api/app/helpers/response_formatter.go
+++
@@ -1,8 +0,0 @@
-package helpers
-
-type ResponseFormatter struct {
-	Code    int         `json:"code"`
-	Status  string      `json:"status"`
-	Message string      `json:"message"`
-	Data    interface{} `json:"data"`
-}
--- vendor/github.com/hpazk/go-echo-rest-api/app/models/products/category_model.go
+++
@@ -1,7 +0,0 @@
-package product
-
-type Category struct {
-	ID          int `gorm:"primary_key;"`
-	Name        string
-	Description string
-}
